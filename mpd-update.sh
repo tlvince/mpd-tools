569401cbf89b541f497ce1da82fdb6b7d0703dc5
Tom Vincent <git@tlvince.com>
2010-08-22 18:29:02 +0800

    Remove polling and quieten mpc

--- mpd-update.sh
+++ mpd-update.sh
@@ -1,17 +1,13 @@
 #!/bin/bash
-# Title:    mpd-update.sh
-# Author:   Tom Vincent
-# Version:  major.minor.revision (YYYY-MM-DD)
-#
-# Source: http://bbs.archlinux.org/viewtopic.php?pid=341957
 #
 # Update the MPD database, add all songs and play one at random.
-mpc crop
-mpc update > /dev/null
-while [ $(mpc | grep Updating | wc -l) == 1 ]; do
-        sleep 1
-done
-mpc clear
-mpc ls | mpc add > /dev/null
-mpc random on
-mpc play
+#
+# Author:   Tom Vincent
+# Created:  2008-01-17
+
+mpc -q crop 2>/dev/null     # Don't care if mpd is stopped
+mpc -q update / --wait      # XXX: check it is actually updating
+mpc -q clear
+mpc ls | mpc -q add
+mpc -q random on
+mpc -q play
