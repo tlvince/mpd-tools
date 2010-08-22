cec9b9d6004f9a4c822059fe43f95d80784c5409
Tom Vincent <git@tlvince.com>
2010-08-22 17:30:23 +0800

    Rewrite since mpc no longer outputs track number

--- mpd-song.sh
+++ mpd-song.sh
@@ -1,12 +1,20 @@
 #!/bin/sh
-# Title:    mpd-playlist.sh
-# Author:   Tom Vincent
-# Version:  0.1.0 (2009-03-01)
-# Source:   http://mpd.wikia.com/wiki/Hack:Mpc-dmenu
-#
 # Search the current MPD playlist for a song and play it.
 #
-# Original version:
-# urxvtc -T "MPD-playlist" -e 'mpc playlist; echo "$line"; mpc; sleep 3'
+# Author:   Tom Vincent
+# Created:  2009-03-01
 
-mpc play `mpc playlist | dmenu -i -b -nb "#000" -nf "#7af" -sb "#000" -sf "#bdf" -p "Song:" | cut -d')' -f1`
+##
+# Create a menu of songs in the current mpd playlist. 
+#
+getChoice()
+{
+    BASE="$(dirname "$(readlink -f "$0")")"     # Handle symlinks
+    . "$BASE/lib/dmenu.sh" 
+
+    mpc playlist | dmenu -p "Song:"
+}
+
+
+# Use sed to find the playlist number
+mpc play $(mpc playlist | sed -ne "/^$(getChoice)$/=")
