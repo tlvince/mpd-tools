d1fe89eb7153e1e7f7405f182000720d43a50811
Tom Vincent <git@tlvince.com>
2010-08-22 18:01:04 +0800

    Complete rewrite

--- mpd-artist.sh
+++ mpd-artist.sh
@@ -1,23 +1,29 @@
 #!/bin/bash
-# Title:    mpd-artist.sh
-# Author:   Tom Vincent
-# Version:  0.1.1 (2009-08-21)
-# Created:  0.1.0 (2009-03-01)
-#
-# Original Author:  Windowlicker
-# Source:           http://bbs.archlinux.org/viewtopic.php?id=61017
 #
 # Search for an artist name and begin playing all songs by that artist.
+#
+# Author:   Tom Vincent
+# Created:  0.1.0 (2009-03-01)
 
-CMD=`mpc ls | dmenu -i -b -nb "#000" -nf "#7af" -sb "#000" -sf "#bdf" -p "Artist:" | cut -d')' -f1`
+# Global variables:
+BASE="$(dirname "$(readlink -f "$0")")"     # Handle symlinks
 
-# TODO: do this better!
-if [ -z "$CMD" ]; then
-    exit
-fi
+# Imports:
+. "$BASE/lib/dmenu.sh"
+. "$BASE/lib/isRunning.sh"
+. "$BASE/lib/logging.sh"
 
-if [ -d /media/akasa/music/"$CMD" ]; then
-  mpc clear & mpc add "$CMD"
-  mpc random on
-  mpc play
-fi
+##
+# Create a menu of all the artists in music directory.
+#
+getChoice()
+{
+    mpc ls | dmenu -p "Artist:"
+}
+
+isRunning mpd || die "Please launch mpd"
+
+CHOICE=$(getChoice) || exit 2
+mpc -q clear && mpc -q add "$CHOICE"
+mpc -q play
+mpc playlist
