f2db024d9bac1b21a661128622ab3a3dd00e8756
Tom Vincent <git@tlvince.com>
2010-09-22 22:08:01 +0800

    Access the eugene db directly:
    * Reduce the amount of loops
    * Around 1/3 running time :)

--- eugene-to-txt.py
+++ eugene-to-txt.py
@@ -4,24 +4,16 @@ import os
 import sqlite3
 
 DB = os.path.expanduser("~") + "/.mpdcron/stats.db"
-IN = os.path.expanduser("~") + "/eugene-plays"
 OUT = os.path.expanduser("~") + "/plays.log"
 PREFIX = "1970-01-01T00:00:00+0000"
 
-songs = []
-with open(IN) as file:
-    for line in file:
-        arr = line.split(":")
-        tup = (arr[0], arr[2].rstrip('\n'))
-        songs.append(tup)
+conn = sqlite3.connect(DB)
+c = conn.cursor()
+c.execute("SELECT artist,title,play_count FROM song WHERE play_count !='0'")
+songs = (c.fetchall())
 
 with open(OUT, mode='w', encoding='utf-8') as outFile:
-    conn = sqlite3.connect(DB)
-    c = conn.cursor()
-
     for song in songs:
-        c.execute('SELECT artist,title FROM song WHERE id=' + song[0])
-        track = c.fetchone()
-        for i in range(int(song[1])):
-            outFile.write(PREFIX + " " + track[0] + " - " + track[1] + "\n")
-
+        for i in range(int(song[2])):
+            outFile.write(PREFIX + " " + song[0] + " - " + song[1] + "\n")
+    conn.close
