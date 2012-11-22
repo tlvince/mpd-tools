13687d253f598c3e8e2989565ef4d89923add93d
Tom Vincent <git@tlvince.com>
2012-11-22 22:22:56 +0000

    Remove crusty old scripts, relicense

--- lib/dmenu.sh
+++
@@ -1,7 +0,0 @@
-##
-# Override dmenu with some options to be used globally in mpd-tools.
-#
-dmenu()
-{
-    $(which dmenu) -b -nb "#000" -nf "#7af" -sb "#000" -sf "#bdf" "$@"
-}
--- lib/isRunning.sh
+++
@@ -1,7 +0,0 @@
-##
-# @return 0 if $1 is running, otherwise 1.
-#
-isRunning()
-{
-    pgrep ^$1$ > /dev/null
-}
